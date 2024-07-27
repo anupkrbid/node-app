feecb5f07a0ac5e3cd995fda911f5d133e0abc17
Anup Kumar Bid <anup@spotdraft.com>
2024-07-28 02:18:49 +0530

    handling post request

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In First Middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="post"><input name="product"><button type="submit">Submit</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In Second Middleware");
-  res.send("Simple node app");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-module.exports = app;
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.listen(3000);
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/**
+ * One of running the server is to export the express app from app.js
+ * and pass it here in the http.createServer() method, make sure to not
+ * listen to the port in the app.js in that case
+ */
+
 const http = require("http");
 
 const app = require("./app");

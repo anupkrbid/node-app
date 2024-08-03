1bd1f34d43b6ec016dc9c7f26b18e3dcc84a9a6b
Anup Kumar Bid <anup@spotdraft.com>
2024-08-03 15:54:34 +0530

    static page serving

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const expHbs = require("express-handlebars");
 
 const appRoutes = require("./app.routes");
 const rootDir = require("./utils/root-dir");
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
+
+app.engine("hbs", expHbs());
+app.set("view engine", "hbs");
+app.set("views", path.join(rootDir, "views"));
 
 app.use(appRoutes);
 
--- src/app.routes.js
+++ src/app.routes.js
@@ -15,7 +15,8 @@ appRouter.get("/", (req, res, next) => {
 
 appRouter.use((req, res, next) => {
   console.log(rootDir);
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  // res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { title: "Page Not Found" });
 });
 
 module.exports = appRouter;
